2f5025f399948e724fa02aae612ad1078207e0c4
yuzen9622 <oscar48079@gmail.com>
2025-12-04 17:53:43 +0800

    first commit

--- config/admin.ts
+++ config/admin.ts
@@ -1,20 +1,26 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("TRANSFER_TOKEN_SALT"),
     },
   },
   secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
+    encryptionKey: env("ENCRYPTION_KEY"),
+  },
+  preview: {
+    enabled: true,
+    config: {
+      allowedOrigins: env("CLIENT_URL"),
+    },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
   },
 });
