93832febc9c0c32aae46aaac9be4fea42e6953fc
yuzen9622 <oscar48079@gmail.com>
2025-12-07 00:14:56 +0800

    feat: update middlewares and add cross-env dependency

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,13 +1,1 @@
-export default () => ({
-  graphql: {
-    config: {
-      shadowCRUD: true,
-      endpoint: "/graphql", // <— single GraphQL endpoint
-      subscriptions: false,
-      maxLimit: -1,
-      apolloServer: {},
-      v4CompatibilityMode:
-        process.env.STRAPI_GRAPHQL_V4_COMPATIBILITY_MODE ?? false,
-    },
-  },
-});
+export default () => ({});
