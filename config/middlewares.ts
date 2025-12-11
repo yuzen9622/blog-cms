2b4cd5f8160d6956f6c5b13765ab2c41d057cabe
yuzen9622 <oscar48079@gmail.com>
2025-12-11 23:39:10 +0800

    feat: update security middleware configuration and add Cloudinary upload provider

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,10 +1,36 @@
 export default [
   "strapi::logger",
   "strapi::errors",
-  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "res.cloudinary.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "res.cloudinary.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   {
     name: "strapi::cors",
     config: {
+      maxAge: 3600,
       origin: ["http://localhost:5173", "https://www.yuzen.dev"],
       headers: ["Content-Type", "Authorization", "Origin", "Accept"],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
